d5e6a16e0576b053e0bda6265bcd40b83373ec78
Marcelo Ramos <marceloveronezramos@hotmail.com>
2018-10-24 21:21:45 -0300

    Novas infos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+
+    addCardSUS() {
+      const prompt = this.alertCtrl.create({
+        title: 'Adicionar cartão SUS',
+        message: "Entre com o cartão SUS do paciente",
+        inputs: [
+          {
+            name: 'Número do cartão',
+            placeholder: 'Número'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Adicionar',
+            handler: data => {
+              console.log('Saved clicked');
+            }
+          }
+        ]
+      });
+      prompt.present();
+    }
 
 }
